8c75db8ac09ea1693c1ed16555fe00ed11c66039
Hernan Meydac <hernan.meydac@auth0.com>
2016-06-07 00:20:49 -0300

    Adds functions to assign and clear concierge

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var fs = require('fs');
 
+var conciergeFile = '/etc/concierge.json';
+
 module.exports = {
   callConcierge: function(req, res) {
     try {
-      var list = fs.readFileSync(path.join(__dirname, 'list/concierge.json'));
+      var list = fs.readFileSync(conciergeFile);
       list = JSON.parse(list);
 
       if (!list[req.channel.name]) {
@@ -13,9 +15,54 @@ module.exports = {
 
       var link = 'https://auth0.slack.com/archives/' + req.channel.name + '/p' + req.message.timestamp.replace('.', '');
       var conciergeMessage = '@' + req.from.name + ' needs your attention in #' + req.channel.name + ' (' + link + ') \n\n*Message*:\n';
-      return res.text(conciergeMessage + req.message.value.text, list[req.channel.name]).send();
+      res.text(conciergeMessage + req.message.value.text, list[req.channel.name]);
+
+      return res.text('A message has been sent to the concierge.').send();
     } catch (e) {
       return res.text('An error has occurred while trying to contact the concierge.\n```' + JSON.stringify(e) + '```').send();
+    }
+  },
+  whosConcierge: function(req, res) {
+    try {
+      var list = fs.readFileSync(conciergeFile);
+      list = JSON.parse(list);
+
+      var conciergeName = list[req.channel.name];
+      if (conciergeName) {
+        return res.text('The concierge for this channel is `' + conciergeName + '`').send();
+      }
+
+      return res.text('This channel does not have a concierge assigned.').send();
+    } catch (e) {
+      return res.text('An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```').send();
+    }
+  },
+  assignConcierge: function(req, res) {
+    try {
+      var list = fs.readFileSync(conciergeFile);
+      list = JSON.parse(list);
+
+      list[req.channel.name] = '@' + req.from.name;
+      fs.writeFileSync(conciergeFile, JSON.stringify(list, null, 2));
+      return res.text('You have been assigned concierge for this channel.').send();
+    } catch (e) {
+      return res.text('An error has occurred while trying to assign the concierge.\n```' + JSON.stringify(e) + '```').send();
+    }
+  },
+  clearConcierge: function(req, res) {
+    try {
+      var list = fs.readFileSync(conciergeFile);
+      list = JSON.parse(list);
+
+      if (list[req.channel.name]) {
+        delete list[req.channel.name];
+        fs.writeFileSync(conciergeFile, JSON.stringify(list, null, 2));
+        return res.text('Unassigned concierge for this channel.').send();
+      }
+
+      return res.text('This channel does not have a concierge assigned. Nothing changed.').send();
+    } catch (e) {
+      return res.text('An error has occurred while trying to unassign the concierge.\n```' + JSON.stringify(e) + '```').send();
     }
   }
 };
